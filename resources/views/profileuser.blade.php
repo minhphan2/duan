585b5fb3a666b0bd45821b5144371e429f564c86
minhphan2 <dbrr231104@gmail.com>
2025-05-27 22:38:45 +0700

    ok thiếu up host và .htaccess

--- resources/views/profileuser.blade.php
+++ resources/views/profileuser.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
-<html lang="vi">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="./images/logo_cake_1-removebg-preview.png" type = "image/x-icon"> <!--FAVICON-->
+    <title>Thông tin cá nhân</title>
 <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+<link rel="icon" href="./images/logo_cake_1-removebg-preview.png" type = "image/x-icon"> <!--FAVICON-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
